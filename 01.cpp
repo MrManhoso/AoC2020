e731aa3b237e6b4459b37a6a8b7954081ca18e22
Erik <erik.jonsson@hiq.se>
2020-12-01 10:03:16 +0100

    added cpp solution to second part of day 1

--- 01.cpp
+++ 01.cpp
@@ -5,6 +5,7 @@
 #include <utility>
 #include <cassert>
 #include <algorithm>
+#include <tuple>
 
 template<typename I>
 void get_expense_report(I i)
@@ -20,24 +21,47 @@ void get_expense_report(I i)
     }
 }
 
-std::pair<int, int> get_2020_nrs(std::vector<int>& expense_report)
+// asserts sorted
+std::pair<int, int> get_tot_as_2nrs(const std::vector<int>& expense_report, int tot)
 {
-    sort(std::begin(expense_report), std::end(expense_report));
     for(std::vector<int>::const_iterator it = std::begin(expense_report); it != std::end(expense_report); ++it){
-        auto x = 2020 - (*it);
+        auto x = tot - (*it);
         if(x < 0) continue;
         if(std::binary_search(std::begin(expense_report), std::end(expense_report), x)) return std::make_pair(*it, x);
     }
     return std::make_pair(-1, -1);
 }
 
+// asserts sorted
+std::tuple<int, int, int> get_tot_as_3nrs(const std::vector<int>& expense_report, int tot)
+{
+    for(std::vector<int>::const_iterator it = std::begin(expense_report); it != std::end(expense_report); ++it){
+        auto sub_tot = tot - (*it);
+        if(sub_tot < 0) continue;
+        // TODO get partition of numbers smaller than sub_tot
+        auto lb = std::lower_bound(std::begin(expense_report), std::end(expense_report), sub_tot);
+        if(lb == std::begin(expense_report)) continue;
+        auto p = get_tot_as_2nrs(std::vector<int>(std::begin(expense_report), lb), sub_tot);
+        if(p.first != -1) return std::make_tuple(*it, p.first, p.second);
+    }
+    return std::make_tuple(-1, -1, -1);
+}
+
 int main()
 {
     std::vector<int> expense_report;
     get_expense_report(std::back_inserter(expense_report));
-    auto nrs = get_2020_nrs(expense_report);
-    assert(nrs.first + nrs.second == 2020);
-    std::cout << nrs.first << ' ' << nrs.second << '\n';
-    std::cout << nrs.first * nrs.second << '\n';
+    sort(std::begin(expense_report), std::end(expense_report));
+    auto nrs2 = get_tot_as_2nrs(expense_report, 2020);
+    assert(nrs2.first + nrs2.second == 2020);
+    std::cout << nrs2.first << ' ' << nrs2.second << '\n';
+    std::cout << "Two numbers: " << nrs2.first * nrs2.second << '\n';
+
+    int first, second, third;
+    std::tie(first, second, third) = get_tot_as_3nrs(expense_report, 2020);
+    assert(first + second + third == 2020);
+    std::cout << first << ' ' << second << ' ' << third << '\n';
+    std::cout << "Three numbers: " << first * second * third << '\n';
+
     return 0;
 }
